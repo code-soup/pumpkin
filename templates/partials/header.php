1ff77580dcc1b97d94d453fc1da8ea59929fd984
piqusy <ivanoramljak@gmail.com>
2019-11-01 21:26:17 +0100

    render nav-ofcanvas only if WP menu has mobile nav

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,13 +1,16 @@
 <header class="banner">
 
-	<button type="button" class="navbar-toggle" data-toggle="offcanvas">
-		<span class="hamburger"></span>
-    	<span class="sr-only">Toggle navigation</span>
-	</button>
-
 	<?php if ( has_nav_menu('nav_primary') ) : ?>
 	    <nav class="navbar-main">
-	    	<?php wp_nav_menu(['theme_location' => 'nav_primary', 'menu_class' => 'nav nav-main', 'container' => false] ); ?>
+	    	<?php wp_nav_menu( ['theme_location' => 'nav_primary', 'menu_class' => 'nav nav-main', 'container' => false] ); ?>
 	    </nav>
 	<?php endif; ?>
+
+	<?php if ( has_nav_menu('nav_offcanvas') ) : ?>
+		<button type="button" class="navbar-toggle" data-toggle="offcanvas">
+			<span class="hamburger"></span>
+			<span class="sr-only">Toggle navigation</span>
+		</button>
+	<?php endif; ?>
+
 </header>
