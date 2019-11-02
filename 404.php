93b473e40e9b954906f730b4d17e57d724c5e926
piqusy <ivanoramljak@gmail.com>
2019-11-02 13:20:15 +0100

    404 template update

--- 404.php
+++ 404.php
@@ -1,7 +1,15 @@
 <?php get_template_part('templates/partials/page', 'header'); ?>
 
-<div class="alert alert-warning">
-	<?php _e('Sorry, but the page you were trying to view does not exist.', 'froots'); ?>
-</div>
 
-<?php get_search_form(); ?>
+<div class="error404-content">
+	<div class="container">
+
+		<div class="alert alert-warning">
+			<?php _e('Sorry, but the page you were trying to view does not exist.', 'pumpkin'); ?>
+			<a href="<?php echo home_url('/') ?>" class="btn">Go Home</a>
+		</div>
+
+		<?php get_search_form(); ?>
+
+	</div>
+</div>
