1c73abd03f08c63876e114411a38abc366580ff4
Ivan Ramljak <ivanoramljak@gmail.com>
2020-12-08 22:37:20 +0100

    close #20 ACF Composer installer

--- functions.php
+++ functions.php
@@ -1,32 +1,41 @@
-<?php if ( ! defined('ABSPATH') ) exit;
+<?php if ( ! defined( 'ABSPATH' ) ) {
+	exit;}
 
 
-if ( ! defined('WP_ENVIRONMENT_TYPE') )
-	define('WP_ENVIRONMENT_TYPE', 'production');
+if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) {
+	define( 'WP_ENVIRONMENT_TYPE', 'production' );
+}
 
 
-define( 'CS_ACF_PATH',  'wp-content/plugins/advanced-custom-fields-pro/' );
+define( 'CS_ACF_PATH', 'vendor/advanced-custom-fields/advanced-custom-fields-pro/' );
 define( 'CS_ACF_ABSPATH', trailingslashit( get_template_directory() ) . CS_ACF_PATH );
 
 /**
  * Include ACF
  */
-if ( ! class_exists('ACF') && file_exists( CS_ACF_ABSPATH . 'acf.php' ) ):
+if ( ! class_exists( 'ACF' ) && file_exists( CS_ACF_ABSPATH . 'acf.php' ) ) :
 
-    require_once CS_ACF_ABSPATH . 'acf.php';
+	require_once CS_ACF_ABSPATH . 'acf.php';
 
-    // Tweak paths
-    add_filter('acf/settings/dir', function ($dir) {
-        return trailingslashit( get_template_directory_uri() ) . CS_ACF_PATH;
-    });
+	// Tweak paths
+	add_filter(
+		'acf/settings/dir',
+		function ( $dir ) {
+			return trailingslashit( get_template_directory_uri() ) . CS_ACF_PATH;
+		}
+	);
 
-    add_filter('acf/settings/path', function ($path) {
-        return CS_ACF_ABSPATH;
-    });
+	add_filter(
+		'acf/settings/path',
+		function ( $path ) {
+			return CS_ACF_ABSPATH;
+		}
+	);
 
-    // Disable ACF menu if not in a development env
-    if (WP_ENVIRONMENT_TYPE !== 'development')
-        add_filter('acf/settings/show_admin', '__return_false');
+	// Disable ACF menu if not in a development env
+	if ( WP_ENVIRONMENT_TYPE !== 'development' ) {
+		add_filter( 'acf/settings/show_admin', '__return_false' );
+	}
 
 endif;
 
@@ -34,8 +43,9 @@ endif;
 /**
  * Load Composer
  */
-if ( locate_template('vendor/autoload.php') )
-	require_once locate_template('vendor/autoload.php');
+if ( locate_template( 'vendor/autoload.php' ) ) {
+	require_once locate_template( 'vendor/autoload.php' );
+}
 
 
 
@@ -44,45 +54,48 @@ if ( locate_template('vendor/autoload.php') )
  *
  * @link( https://github.com/soberwp/models, documentation )
  */
-add_filter('sober/models/path', function() {
+add_filter(
+	'sober/models/path',
+	function() {
 
-    return trailingslashit( get_stylesheet_directory() ) . 'inc/models';
-});
+		return trailingslashit( get_stylesheet_directory() ) . 'inc/models';
+	}
+);
 
 /**
  * Theme includes
  */
-$includes = [
-    'utils/helpers',
+$includes = array(
+	'utils/helpers',
 	'utils/assets',
 	'utils/wrapper',
 	'setup/class.cs',
 	'setup/acf-options',
 	'setup/acf-load-fields',
 	'setup/acf-widget',
-    'wp-mods/wp-admin',
-    'wp-mods/wp-cleanup',
-    'wp-mods/wp-core',
-    'wp-mods/wp-login',
+	'wp-mods/wp-admin',
+	'wp-mods/wp-cleanup',
+	'wp-mods/wp-core',
+	'wp-mods/wp-login',
 	'components/class.functions',
 	'components/class.ajax',
 	'components/class.user',
 	'plugin-mods/gravity-forms',
-];
+);
 
 
 // Woocommerce
-if ( function_exists('is_woocommerce') )
+if ( function_exists( 'is_woocommerce' ) ) {
 	$includes[] = 'plugin-mods/woocommerce/tweaks';
+}
 
 
 
 foreach ( $includes as $file ) {
 
-    if ( locate_template('inc/' . $file . '.php') ) {
-	   require_once locate_template('inc/' . $file . '.php');
-    }
-    else {
-        echo $file;
-    }
+	if ( locate_template( 'inc/' . $file . '.php' ) ) {
+		require_once locate_template( 'inc/' . $file . '.php' );
+	} else {
+		echo $file;
+	}
 }
